4252f6e1446d95fa1fe73e4f44cc6e4c7ca77dfc
lyam131103 <lykhacam207@gmail.com>
2025-03-03 10:56:12 +0700

    add screen4 event pause-play

--- app/src/main/java/com/example/myapplication/Screen4Activity.kt
+++ app/src/main/java/com/example/myapplication/Screen4Activity.kt
@@ -3,52 +3,74 @@ package com.example.myapplication
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.os.PersistableBundle
 import android.widget.ImageButton
 import android.widget.SeekBar
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.databinding.ActivityMainBinding
+import java.util.Timer
+import java.util.TimerTask
+
 class Screen4Activity : AppCompatActivity(){
 
     private var isPlaying = false
     private var  isLike = false
     private var currentProgress = 0
-    private val totalTime = 194 // Ví dụ: 180 giây (3 phút)
-    private val handler = Handler(Looper.getMainLooper())
-
-    private lateinit var sbPlay: SeekBar
-    private lateinit var btnPlay: ImageButton
-    private lateinit var tvCurrentTime: TextView
-    private lateinit var tvTotalTime: TextView
-    private lateinit var imgLike: ImageButton
+    private val TOTAL_TIME = 194 // Ví dụ: 180 giây (3 phút)
+    private var timer: Timer? = null
+//    private val handler = Handler(Looper.getMainLooper())
 
 
+//    private lateinit val sbPlay by lazy { finishActivity(R.id.sbPlay) } //chỉ dùng với var?
+//    private lateinit var btnPlay: ImageButton
+//    private lateinit var tvCurrentTime: TextView
+//    private lateinit var tvTotalTime: TextView
+//    private lateinit var imgLike: ImageButton
+
+//    private lateinit var binding: ActivityMainBinding
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+//        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        sbPlay = findViewById(R.id.sbPlay)
-        btnPlay = findViewById(R.id.btnPlay)
-        tvCurrentTime = findViewById(R.id.tvCurrentTime)
-        tvTotalTime = findViewById(R.id.tvTotalTime)
-        imgLike = findViewById(R.id.imgLike)
 
-        sbPlay.max = totalTime
-        tvCurrentTime.text = formatTime(0)
-        tvTotalTime.text = formatTime(totalTime)
+//        sbPlay = findViewById(R.id.sbPlay)
+//        btnPlay = findViewById(R.id.btnPlay)
+//        tvCurrentTime = findViewById(R.id.tvCurrentTime)
+//        tvTotalTime = findViewById(R.id.tvTotalTime)
+//        imgLike = findViewById(R.id.imgLike)
 
-        imgLike.setOnClickListener{
-            if(isLike){
-                unLike()
+        binding.sbPlay.max = TOTAL_TIME
+        binding.tvCurrentTime.text = formatTime(0)
+        binding.tvTotalTime.text = formatTime(TOTAL_TIME)
+
+//        binding.imgLike.setOnClickListener{
+//            if(isLike){
+//                unLike()
+//            }
+//            else{
+//                enterLike()
+//            }
+//        }
+
+        binding.cbLike.setOnCheckedChangeListener{_, isChecked->
+            if(isChecked){
+                binding.cbLike.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_heart_full,0,0,0)
             }
             else{
-                enterLike()
+                binding.cbLike.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_heart_image,0,0,0)
             }
+
         }
+
         // Xử lý khi nhấn nút Play/Pause
-        btnPlay.setOnClickListener {
+        binding.btnPlay.setOnClickListener {
             if (isPlaying) {
                 clickPause()
             } else {
@@ -56,37 +78,82 @@ class Screen4Activity : AppCompatActivity(){
             }
         }
     }
-    private val updateRun = object : Runnable {
-        override fun run() {
-            if (isPlaying && currentProgress < totalTime) {
-                currentProgress++
-                sbPlay.progress = currentProgress
-                tvCurrentTime.text = formatTime(currentProgress)
-                handler.postDelayed(this, 1000)
-            } else {
-                clickPause()
-            }
+//    private val updateRun = object : Runnable {
+//        override fun run() {
+//            if (isPlaying && currentProgress < TOTAL_TIME) {
+//                currentProgress++
+//                binding.sbPlay.progress = currentProgress
+//                binding.tvCurrentTime.text = formatTime(currentProgress)
+//                handler.postDelayed(this, 1000)
+//            } else {
+//                clickPause()
+//            }
+//        }
+//    }
+
+
+//    phục hồi dữ liệu
+    companion object{
+        const val KEY_PROGRESS = "KEY_PROGRESS"
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putInt(KEY_PROGRESS,currentProgress)
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        currentProgress = savedInstanceState.getInt(KEY_PROGRESS)
+        binding.tvCurrentTime.text = formatTime(currentProgress)
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        clickPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if(isLike){
+            clickPlay()
         }
     }
 
-    private fun enterLike(){
-        isLike = true
-        imgLike.setImageResource(R.drawable.ic_heart_full)
-    }
-    private fun unLike(){
-        isLike = false
-        imgLike.setImageResource(R.drawable.ic_heart_image)
-    }
+//    private fun enterLike(){
+//        isLike = true
+//        binding.cbLike.setImageResource(R.drawable.ic_heart_full)
+//    }
+//    private fun unLike(){
+//        isLike = false
+//        binding.cbLike.setImageResource(R.drawable.ic_heart_image)
+//    }
+
     private fun clickPlay() {
         isPlaying = true
-        btnPlay.setImageResource(R.drawable.pause)
-        handler.post(updateRun)
+        binding.btnPlay.setImageResource(R.drawable.pause)
+
+        timer = Timer()
+        timer?.schedule(object : TimerTask() {
+            override fun run() {
+                runOnUiThread {
+                    if (isPlaying && currentProgress < TOTAL_TIME) {
+                        currentProgress++
+                        binding.sbPlay.progress = currentProgress
+                        binding.tvCurrentTime.text = formatTime(currentProgress)
+                    } else {
+                        clickPause()
+                    }
+                }
+            }
+        }, 0, 1000)
     }
 
     private fun clickPause() {
         isPlaying = false
-        btnPlay.setImageResource(R.drawable.play)
-        handler.removeCallbacks(updateRun)
+        binding.btnPlay.setImageResource(R.drawable.play)
+        timer?.cancel()
     }
 
     private fun formatTime(seconds: Int): String {
