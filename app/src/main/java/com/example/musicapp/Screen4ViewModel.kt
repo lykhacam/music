59c909dc15936a05b6be9a32befb953a2ba4610b
lyam131103 <lykhacam207@gmail.com>
2025-03-12 11:24:51 +0700

    Replace old project with new one

--- app/src/main/java/com/example/musicapp/Screen4ViewModel.kt
+++ app/src/main/java/com/example/musicapp/Screen4ViewModel.kt
@@ -9,51 +9,50 @@ class Screen4ViewModel(private val state: SavedStateHandle) : ViewModel() {
 
     companion object {
         private const val KEY_PROGRESS = "KEY_PROGRESS"
+        private const val KEY_IS_PLAYING = "KEY_IS_PLAYING"
     }
 
     private val _currentProgress = MutableLiveData(state.get(KEY_PROGRESS) ?: 0)
     val currentProgress: LiveData<Int> = _currentProgress
 
-    private val _isPlaying = MutableLiveData(false)
+    private val _isPlaying = MutableLiveData(state.get(KEY_IS_PLAYING) ?: false)
     val isPlaying: LiveData<Boolean> = _isPlaying
 
     private val _isLiked = MutableLiveData(false)
     val isLiked: LiveData<Boolean> = _isLiked
 
     private var timer: Timer? = null
-    private val totalTime = 180  // Giới hạn thời gian (ví dụ: 3 phút)
+    private val totalTime = 180  // 3 phút (tổng thời gian)
+
+    init {
+        if (_isPlaying.value == true) startTimer()
+    }
+
+    fun setPlaying(play: Boolean) {
+        _isPlaying.value = play
+        state[KEY_IS_PLAYING] = play
+        if (play) startTimer() else stopTimer()
+    }
 
     fun updateProgress(progress: Int) {
         _currentProgress.value = progress
         state[KEY_PROGRESS] = progress
     }
 
-    fun setPlaying(isPlaying: Boolean) {
-        _isPlaying.value = isPlaying
-        if (isPlaying) {
-            startTimer()
-        } else {
-            stopTimer()
-        }
-    }
-
     fun toggleLike() {
-        _isLiked.value = _isLiked.value != true
+        _isLiked.value = !(_isLiked.value ?: false)
     }
 
     private fun startTimer() {
-        if (timer == null) {
-            timer = Timer()
-            timer?.schedule(object : TimerTask() {
-                override fun run() {
-                    _currentProgress.postValue((_currentProgress.value ?: 0) + 1)
-                    if (_currentProgress.value ?: 0 >= totalTime) {
-                        stopTimer()
-                    }
-                }
-            }, 0, 1000) // Chạy mỗi giây
-        }
-
+        stopTimer() // Dừng timer cũ trước khi khởi tạo mới
+        timer = Timer()
+        timer?.scheduleAtFixedRate(object : TimerTask() {
+            override fun run() {
+                val newProgress = (_currentProgress.value ?: 0) + 1
+                if (newProgress >= totalTime) stopTimer()
+                _currentProgress.postValue(newProgress)
+            }
+        }, 1000, 1000)
     }
 
     private fun stopTimer() {
@@ -62,7 +61,9 @@ class Screen4ViewModel(private val state: SavedStateHandle) : ViewModel() {
     }
 
     override fun onCleared() {
-        super.onCleared()
         stopTimer()
+        super.onCleared()
     }
+
+    fun getTotalTime() = totalTime
 }
